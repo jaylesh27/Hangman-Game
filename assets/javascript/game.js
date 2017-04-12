df267e6914f54260e70aa35024c1ab23434b9237
Jay Patel <Jay@Jay-SurfacePro3.(none)>
2017-04-11 20:20:04 -0400

    minor progress

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+var hangmanAnswers = {
+	"WRANGELLSTELIAS",
+	"GATESOFTHEARCTIC",
+	"DENALI",
+	"KATMAI",
+	"DEATHVALLEY",
+	"GLACIERBAY",
+	"LAKECLARK",
+	"YELLOWSTONE",
+	"KOBUKVALLEY",
+	"EVERGLADES",
+	"GRANDCANYON",
+	"GLACIER",
+	"OLYMPIC",
+	"BIGBEND",
+	"JOSHUATREE",
+	"YOSEMITE",
+	"KENAIFJORDS",
+	"ISLEROYALE",
+	"THEGREATSMOKYMOUNTAINS",
+	"NORTHCASCADES"
+};
+
+var hangman = {
+
+	document.onkeyup = function(event) {
+		var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+	}
+
+
+
+
+
+
+
+
+}
